a0448a963a8b203690dbce43402f9e815f17704c
Dino Morelli <dino@ui3.info>
2017-11-03 17:55:55 +0000

    Added getOpenOrders Bittrex API call and data types

--- src/Main.hs
+++ src/Main.hs
@@ -9,15 +9,15 @@ import Network.Exchange.Bittrex
 
 main :: IO ()
 main = do
-   {-
-   creds <- ((read <$> readFile "/home/dino/.config/crobot/bittrex.creds") :: IO BittrexCreds)
-   print creds
-   -}
-
    let mktBTC_LTC = Market "BTC" "LTC"
-   printf "\nmarket_getTicker for %s\n" (show mktBTC_LTC)
+   printf "\ngetTicker for %s\n" (show mktBTC_LTC)
    print =<< getTicker mktBTC_LTC
 
    let mktFOO_BAR = Market "FOO" "BAR"
-   printf "\nmarket_getTicker for %s\n" (show mktFOO_BAR)
+   printf "\ngetTicker for %s\n" (show mktFOO_BAR)
    print =<< getTicker mktFOO_BAR
+
+   creds <- ((read <$> readFile "/home/dino/.config/crobot/bittrex.creds") :: IO BittrexCreds)
+
+   putStrLn "\ngetOpenOrders"
+   print =<< getOpenOrders creds
--- src/Network/Exchange/Bittrex.hs
+++ src/Network/Exchange/Bittrex.hs
@@ -3,8 +3,10 @@
 module Network.Exchange.Bittrex
    ( BittrexCreds (..)
    , Market (..)
+   , Order (..)
    , Ticker (..)
 
+   , getOpenOrders
    , getTicker
    )
 where
@@ -12,13 +14,17 @@ where
 import Control.Monad.Except ( runExcept, throwError, unless )
 import Crypto.Hash.SHA512 ( hmac )
 import Data.Aeson hiding ( encode )
-import Data.Aeson.Types ( Options (fieldLabelModifier), defaultOptions, typeMismatch )
+import Data.Aeson.Types (Options (constructorTagModifier,
+   fieldLabelModifier, tagSingleConstructors), camelTo2, defaultOptions, typeMismatch )
 import Data.ByteString.Base16 ( encode )
 import Data.Char ( toLower, toUpper )
 import Data.Maybe ( fromJust )
 import Data.String.Conv ( toS )
 import Data.Text hiding ( head, map, tail, toLower, toUpper )
---import Data.Time ( defaultTimeLocale, formatTime, getCurrentTime )
+import qualified Data.Text as T
+import Data.Time ( UTCTime, defaultTimeLocale, formatTime,
+   getCurrentTime, iso8601DateFormat, parseTimeM )
+--import Debug.Trace ( trace )
 import GHC.Generics ( Generic )
 import Network.Curl ( CurlOption (CurlHttpHeaders), curlGetString )
 import Network.Curl.Code ( CurlCode (CurlOK) )
@@ -32,12 +38,12 @@ baseUri = "https://bittrex.com/api/v1.1"
 
 type Uri = String
 
-type ApiSecret = String
+type ApiSecret = Text
 
 
 data BittrexCreds = BittrexCreds
-   { bcredsApiKey :: Text
-   , bcredsApiSecret :: Text
+   { bcredsApiKey :: ApiSecret
+   , bcredsApiSecret :: ApiSecret
    }
    deriving (Read, Show)
 
@@ -53,9 +59,15 @@ instance FromJSON a => FromJSON (BittrexResponse a)
 
 
 data Market = Market Text Text
+   deriving Generic
 
 instance Show Market where
    show (Market cur1 cur2) = printf "%s-%s" cur1 cur2
+
+instance FromJSON Market where
+   parseJSON = withText "Market" $ \t -> return $ Market
+      (T.takeWhile (/= '-') t)
+      (T.tail . T.dropWhile (/= '-') $ t)
 
 
 newtype Amount = Amount Float
@@ -114,6 +126,136 @@ getTicker market = do
       return . fromJust . result $ br
 
 
+data OrderType
+   = LimitSell
+   deriving (Generic, Show)
+
+instance FromJSON OrderType where
+   parseJSON = genericParseJSON $ defaultOptions
+      { constructorTagModifier = map toUpper . camelTo2 '_'
+      , tagSingleConstructors = True
+      }
+
+
+newtype Timestamp = Timestamp UTCTime
+   deriving (Generic, Show)
+
+instance FromJSON Timestamp where
+   parseJSON = withText "UTCTime" $ \t -> Timestamp <$> (parseTimeM True
+      defaultTimeLocale (iso8601DateFormat $ Just "%H:%M:%S%Q") . toS $ t)
+
+
+data Order = Order
+   { orderUuid :: Text
+   , exchange :: Market
+   , orderType :: OrderType
+   , quantity :: Amount
+   , quantityRemaining :: Amount
+   , limit :: Amount
+   , commissionPaid :: Amount
+   , price :: Amount
+   , pricePerUnit :: Maybe Amount
+   , opened :: Timestamp
+   , closed :: Maybe Timestamp
+   , cancelInitiated :: Bool
+   , immediateOrCancel :: Bool
+   }
+   deriving (Generic, Show)
+
+instance FromJSON Order where
+   parseJSON = withObject "Order" $ \v -> Order
+      <$> v .: "OrderUuid"
+      <*> v .: "Exchange"
+      <*> v .: "OrderType"
+      <*> v .: "Quantity"
+      <*> v .: "QuantityRemaining"
+      <*> v .: "Limit"
+      <*> v .: "CommissionPaid"
+      <*> v .: "Price"
+      <*> v .: "PricePerUnit"
+      <*> v .: "Opened"
+      <*> v .: "Closed"
+      <*> v .: "CancelInitiated"
+      <*> v .: "ImmediateOrCancel"
+
+
+--type Orders = [Order]
+
+
+{- Example market/getopenorders response document
+
+   {
+       "success": true,
+       "message": "",
+       "result": [
+           {
+               "Uuid": null,
+               "OrderUuid": "464a254c-9ecf-47fd-901c-020dd37ad003",
+               "Exchange": "BTC-LGD",
+               "OrderType": "LIMIT_SELL",
+               "Quantity": 89.24166776,
+               "QuantityRemaining": 89.24166776,
+               "Limit": 0.00046335,
+               "CommissionPaid": 0.00000000,
+               "Price": 0.00000000,
+               "PricePerUnit": null,
+               "Opened": "2017-10-11T20:47:19.5",
+               "Closed": null,
+               "CancelInitiated": false,
+               "ImmediateOrCancel": false,
+               "IsConditional": false,
+               "Condition": "NONE",
+               "ConditionTarget": null
+           },
+           {
+               "Uuid": null,
+               "OrderUuid": "fc872c93-5f0e-4060-9a96-60fa0e0cb41d",
+               "Exchange": "BTC-NEO",
+               "OrderType": "LIMIT_SELL",
+               "Quantity": 10.90780889,
+               "QuantityRemaining": 10.90780889,
+               "Limit": 0.00800000,
+               "CommissionPaid": 0.00000000,
+               "Price": 0.00000000,
+               "PricePerUnit": null,
+               "Opened": "2017-10-24T03:39:55.16",
+               "Closed": null,
+               "CancelInitiated": false,
+               "ImmediateOrCancel": false,
+               "IsConditional": false,
+               "Condition": "NONE",
+               "ConditionTarget": null
+           }
+       ]
+   }
+-}
+
+getOpenOrders :: BittrexCreds -> IO (Either String [Order])
+getOpenOrders (BittrexCreds apiKey apiSecret) = do
+   nonce <- formatTime defaultTimeLocale "%s" <$> getCurrentTime
+
+   let uri = printf "%s/market/getopenorders?apikey=%s&nonce=%s" baseUri apiKey nonce
+   (code, rawdoc) <- curlGetString uri $ signUri apiSecret uri
+   --print code
+   --hPutStrLn stderr . toS $ rawdoc
+
+   return . runExcept $ do
+      unless (code == CurlOK) $
+         throwError $ printf "curl call failed, code: %s, document returned:\n%s"
+         (show code) ((toS rawdoc) :: String)
+      br <- maybe (throwError "Unable to parse reply into a BittrexResponse [Order]")
+         return $ decode . toS $ rawdoc
+      unless (success br) $
+         throwError $ "API call unsuccessful, message:\n" ++ (toS . message $ br)
+      return . fromJust . result $ br
+
+
+-- Assuming the API secret is coming from something monadic, possibly impure, so pass it in
+signUri :: ApiSecret -> Uri -> [CurlOption]
+signUri apiSecret uri = [CurlHttpHeaders ["apisign:" ++ (toS . encode $ sign)]]
+   where sign = hmac (toS apiSecret) (toS uri)
+
+
 {-
 main :: IO ()
 main = do
@@ -128,9 +270,3 @@ main = do
    hPutStrLn stderr $ "Got response: " ++ (show . fst $ resp)
    putStrLn . snd $ resp
 -}
-
-
--- Assuming the API secret is coming from something monadic, possibly impure, so pass it in
-signUri :: ApiSecret -> Uri -> [CurlOption]
-signUri apiSecret uri = [CurlHttpHeaders ["apisign:" ++ (toS . encode $ sign)]]
-   where sign = hmac (toS apiSecret) (toS uri)
